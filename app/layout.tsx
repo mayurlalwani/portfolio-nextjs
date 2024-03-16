1e337814220d69623bf89ed7f98377aeb922429d
mayurlalwani <lalwanimayur06@gmail.com>
2024-03-16 15:02:04 +0530

    remove vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
-"use client";
-import Navbar from "@/components/Navbar";
-import "./globals.css";
-import { ThemeProvider } from "next-themes";
-import Footer from "@/components/Footer";
-import Head from "./head";
-import { Analytics } from '@vercel/analytics/react';
+'use client';
+import Navbar from '@/components/Navbar';
+import './globals.css';
+import { ThemeProvider } from 'next-themes';
+import Footer from '@/components/Footer';
+import Head from './head';
 
 export default function RootLayout({
   children,
@@ -12,12 +11,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Analytics />
-    <html lang="en">
+    <html lang='en'>
       <Head />
-      <script defer src="https://eu.umami.is/script.js" data-website-id="0bd010de-069e-437a-8c2a-e739e0eb5583"></script>
-      <body className="dark:bg-stone-900">
-        <ThemeProvider enableSystem={true} attribute="class">
+      <script
+        defer
+        src='https://eu.umami.is/script.js'
+        data-website-id='0bd010de-069e-437a-8c2a-e739e0eb5583'
+      ></script>
+      <body className='dark:bg-stone-900'>
+        <ThemeProvider enableSystem={true} attribute='class'>
           <Navbar />
           {children}
         </ThemeProvider>
